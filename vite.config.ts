95eacd51dba2ea957825416fd72a11e7abb36d7c
Shashank Tiwari <shashank@tiwari.co>
2025-06-03 00:14:43 +0530

    fix: vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {},
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        styles: path.resolve(__dirname, 'src/index.css'), // 🔧 ADD THIS
-      },
     },
   },
 });
