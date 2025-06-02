bbba7d4b5c848c99605c90d708154c6db49f09c2
Shashank Tiwari <shashank@tiwari.co>
2025-06-03 00:34:54 +0530

    new

--- src/components/ui/table.tsx
+++
@@ -1,6 +0,0 @@
-export const Table = ({ children }) => <table>{children}</table>;
-export const TableHead = ({ children, ...props }) => <th {...props}>{children}</th>;
-export const TableHeader = ({ children }) => <thead>{children}</thead>;
-export const TableBody = ({ children }) => <tbody>{children}</tbody>;
-export const TableRow = ({ children }) => <tr>{children}</tr>;
-export const TableCell = ({ children }) => <td>{children}</td>;
