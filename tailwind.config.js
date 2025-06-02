9a333b2f6ae891a6edac283a8734b9ac3381821f
Shashank Tiwari <shashank@tiwari.co>
2025-06-03 00:27:24 +0530

    fix: ensure tailwind compiles correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: '#166534', // ESG green
+        greenbrand: '#166534',
       },
     },
   },
